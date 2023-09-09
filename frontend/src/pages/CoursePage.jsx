d1d52d20d73e6fde14bc699fa0ff372501d38ae5
DELL <nickorovkinwork@gmail.com>
2023-09-09 22:46:49 +0300

    feat: add feed to course page

--- frontend/src/pages/CoursePage.jsx
+++ frontend/src/pages/CoursePage.jsx
@@ -3,12 +3,20 @@ import { useParams } from "react-router-dom";
 import { Container, Box, Typography, Card, CardContent, Grid, TextField, InputAdornment, IconButton } from "@mui/material";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import SendIcon from '@mui/icons-material/Send';
+import AssignmentIcon from '@mui/icons-material/Assignment';
 
 const CoursePage = () => {
     const params = useParams();
 
     //Make API Request
     const course = {id: params.id, name: "11A: PE", teacher: "Sam Sulek", desc: "Daily 3PM"};
+    const content = [{ttype: "Announcement", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"},
+        {ttype: "Task", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"},
+        {ttype: "Announcement", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"},
+        {ttype: "Announcement", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"},
+        {ttype: "Task", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"}
+    ];
+
     return (
         <Container disableGutters maxWidth="lg">
 			<Box sx={{		
@@ -31,31 +39,61 @@ const CoursePage = () => {
                                 <Typography variant="h6" color="text.secondary">
                                     Объявления
                                 </Typography>
-                                <Typography sx={{ mt: 2}} color="text.primary">
+                                <Typography sx={{ mt: 2 }} color="text.primary">
                                     Нет текущих объявлений
                                 </Typography>
                             </CardContent>
                         </Card>
                     </Grid>
                     <Grid item xs={12} sm={8}>
-                        <Card variant="outlined">
-                            <TextField fullWidth placeholder="Оставьте комментарий" 
-                                InputProps={{
-                                    startAdornment: (
-                                        <InputAdornment position="start">
-                                            <AccountCircle color="secondary" />
-                                        </InputAdornment>
-                                    ),
-                                    endAdornment: (
-                                        <InputAdornment position="end">
-                                            <IconButton color="primary">
-                                                <SendIcon />
-                                            </IconButton>
-                                        </InputAdornment>
-                                    )
-                                }}
-                            />
-                        </Card>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Card variant="outlined">
+                                    <TextField fullWidth placeholder="Оставьте комментарий" 
+                                        InputProps={{
+                                            startAdornment: (
+                                                <InputAdornment position="start">
+                                                    <AccountCircle color="secondary" />
+                                                </InputAdornment>
+                                            ),
+                                            endAdornment: (
+                                                <InputAdornment position="end">
+                                                    <IconButton color="primary">
+                                                        <SendIcon />
+                                                    </IconButton>
+                                                </InputAdornment>
+                                            )
+                                        }}
+                                    />
+                                </Card>
+                            </Grid>
+                            <Grid item xs={12}>
+                                {content.map(c =>
+                                    c.ttype === "Announcement"
+                                    ?
+                                        <Card sx={{ mb: 3, border: "1px solid #A8A8A8" }}>
+                                            <CardContent>
+                                                <Typography sx={{ fontSize: 14 }} color="text.disabled" gutterBottom>
+                                                    <AccountCircle sx={{ verticalAlign: "middle", mr: 1 }} />
+                                                    {course.teacher}
+                                                </Typography>
+                                                <Typography color="text.primary">
+                                                    {c.desc}
+                                                </Typography>
+                                            </CardContent>
+                                        </Card>
+                                    :
+                                        <Card sx={{ mb: 3, border: "1px solid #A8A8A8" }}>
+                                            <CardContent>
+                                                <Typography color="text.primary">
+                                                    <AssignmentIcon color="primary" sx={{ verticalAlign: "middle", mr: 1 }} />
+                                                    {course.teacher} опубликовал <b>{c.desc.slice(0, 50) + "..."}</b>
+                                                </Typography>
+                                            </CardContent>
+                                        </Card>
+                                )}
+                            </Grid>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Box>
