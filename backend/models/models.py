67700bfe8018b0fba22da60372780f0423f2a2ff
DELL <nickorovkinwork@gmail.com>
2024-02-29 16:40:43 +0200

    feat: Finish Users CRUD

--- backend/models/models.py
+++ backend/models/models.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class UserModel(BaseModel):
+    userToken: Optional[str] = None
     username: str
     password: str
     name: str
--- backend/routes/user.py
+++ backend/routes/user.py
@@ -4,12 +4,35 @@ sys.path.append("../models")
 from fastapi import APIRouter
 from config.config import EasyEduDB
 from models.models import UserModel
+import uuid
 
 router = APIRouter()
 
-@router.get("/{username}")
-async def getUser(username: str):
-    userData = EasyEduDB.Users.find_one({"username": username}, {"_id": 0, "password": 0})
+@router.post("/")
+async def createUser(userData: UserModel):
+    userData = userData.dict()
+    userData["userToken"] = uuid.uuid4().hex
+    if EasyEduDB.Users.find_one({"username": userData["username"]}) is not None:
+        return {
+            "response-type": "Error",
+            "description": "Username should be unique"
+        }
+    try:
+        EasyEduDB.Users.insert_one(userData)
+        return {
+            "response-type": "Success",
+            "data": userData["userToken"],
+            "description": "User has been successfully created"
+        }
+    except:
+        return {
+            "response-type": "Error",
+            "description": "Error occured during user creation"
+        }
+        
+@router.get("/{userToken}")
+async def readUser(userToken: str):
+    userData = EasyEduDB.Users.find_one({"userToken": userToken}, {"_id": 0, "password": 0})
     if userData is not None:
         return {
             "response-type": "Success",
@@ -21,16 +44,51 @@ async def getUser(username: str):
             "description": "No such user"
         }
 
-@router.post("/")
-async def createUser(userData: UserModel):
+@router.put("/")
+async def updateUser(userData: UserModel):
+    userData = userData.dict()
+    if userData.get("userToken", None) is None:
+        return {
+            "response-type": "Error",
+            "description": "UserToken should be passed"
+        }
+    if EasyEduDB.Users.find_one({"userToken": userData["userToken"]}) is None:
+        return {
+            "response-type": "Error",
+            "description": "User does not exist"
+        }
+    if EasyEduDB.Users.find_one({"userToken": {"$ne": userData["userToken"]}, "username": userData["username"]}) is not None:
+        return {
+            "response-type": "Error",
+            "description": "New username should be unique"
+        }
     try:
-        EasyEduDB.Users.insert_one(userData.dict())
+        EasyEduDB.Users.replace_one({"userToken": userData["userToken"]}, userData)
         return {
             "response-type": "Success",
-            "description": "User has been successfully created"
+            "description": "User has been successfully updated"
         }
     except:
         return {
             "response-type": "Error",
-            "description": "Error occured during user creation"
+            "description": "Error occured during user update"
+        }
+
+@router.delete("/{userToken}")
+async def deleteUser(userToken: str):
+    if EasyEduDB.Users.find_one({"userToken": userToken}) is None:
+        return {
+            "response-type": "Error",
+            "description": "User does not exist"
+        }
+    try:
+        EasyEduDB.Users.delete_one({"userToken": userToken})
+        return {
+            "response-type": "Success",
+            "description": "User has been successfully deleted"
+        }
+    except:
+        return {
+            "response-type": "Error",
+            "description": "Error occured during user deletion"
         }
