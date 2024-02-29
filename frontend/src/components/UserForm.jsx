5963e6e5908b1bd56868fa0a7ac1ddcffc3f7b65
DELL <nickorovkinwork@gmail.com>
2024-02-29 20:51:25 +0200

    feat: Add birthday picker to user form + User signup API request

--- frontend/src/components/UserForm.jsx
+++ frontend/src/components/UserForm.jsx
@@ -1,25 +1,41 @@
 import React from "react";
-import { useState } from "react";
-import { useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../context";
 import { Box, Grid, Typography, TextField, Button } from "@mui/material";
+import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { useNavigate } from "react-router-dom";
 
 const UserForm = ({btnLabel, type}) => {
-    const [userData, setUserData] = useState({name: "", surname: "", username: "", password: ""});
+    const [userData, setUserData] = useState({
+        name: null, surname: null, username: null, password: null, birthday: null
+    });
 	const [error, setError] = useState("");
 	const {username, setUsername} = useContext(AuthContext);
     const navigate = useNavigate();
 
-    const regUser = e => {
+    const regUser = async e => {
 		e.preventDefault();
-		// Make API Request to Register New user
-		for (let k in userData) {
-			if (userData[k] === "") {
+		for (let field in userData) {
+			if (userData[field] === null) {
 				setError("Fill in all the fields");
 				return;
 			}
 		}
+        const formatDate = `${Math.floor(userData.birthday.$D / 10)}${userData.birthday.$D % 10}.${Math.floor((userData.birthday.$M + 1) / 10)}${(userData.birthday.$M + 1) % 10}.${userData.birthday.$y}`;
+        console.log(userData, formatDate);
+        // Make API Request to Register New user
+        const response = await fetch("http://127.0.0.1:8080/user", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8"
+            },
+            body: JSON.stringify({
+                ...userData, birthday: formatDate
+            })
+        });
+        const responseJSON = await response.json();
+        console.log(responseJSON);
 		localStorage.setItem("username", userData.username);
 		setUsername(userData.username);
         if (type === "change") navigate(`/profile/${userData.username}`);
@@ -27,7 +43,7 @@ const UserForm = ({btnLabel, type}) => {
 
     return (
         <Box component="form" noValidate sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
+            <Grid container spacing={2} alignItems="center">
                 {error !== "" &&
                     <Grid sx={{ mb: 2 }} item xs={12}>
                         <Typography sx={{ fontWeight: "bold" }} color="error">
@@ -50,6 +66,13 @@ const UserForm = ({btnLabel, type}) => {
                 <Grid item xs={12}>
                     <TextField required value={userData.password} onChange={e => setUserData({...userData, password: e.target.value})}
                     fullWidth label="Password" type="password" />
+                </Grid>
+                <Grid item xs={12}>
+                    <LocalizationProvider fullWidth dateAdapter={AdapterDayjs}>
+                        <DatePicker required value={userData.birthday}
+                        onChange={e => setUserData({...userData, birthday: e})}
+                        slotProps={{textField: {fullWidth: true}}} label="Birthday" />
+                    </LocalizationProvider>
                 </Grid>
             </Grid>
             <Button
